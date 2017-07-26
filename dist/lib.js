8cde63e9e81d48834c555aefd9e188ab628b8300
Mo Bitar <me@bitar.io>
2017-07-26 18:30:31 -0500

    Properly decode 000 types

--- dist/lib.js
+++ dist/lib.js
@@ -333,6 +333,15 @@ export { SFCryptoWeb }
   }
 
   static decryptItem(item, keys) {
+
+    if((item.content.startsWith("001") || item.content.startsWith("002")) && item.enc_item_key) {
+      // is encrypted, continue to below
+    } else {
+      // is base64 encoded
+      item.content = SFJS.crypto.base64Decode(item.content.substring(3, item.content.length))
+      return;
+    }
+
     // decrypt encrypted key
     var encryptedItemKey = item.enc_item_key;
     var requiresAuth = true;
@@ -388,13 +397,7 @@ export { SFCryptoWeb }
      var isString = typeof item.content === 'string' || item.content instanceof String;
      if(isString) {
        try {
-         if((item.content.startsWith("001") || item.content.startsWith("002")) && item.enc_item_key) {
-           // is encrypted
-           this.decryptItem(item, keys);
-         } else {
-           // is base64 encoded
-           item.content = SFJS.crypto.base64Decode(item.content.substring(3, item.content.length))
-         }
+         this.decryptItem(item, keys);
        } catch (e) {
          item.errorDecrypting = true;
          if(throws) {
--- dist/sfjs.js
+++ dist/sfjs.js
@@ -614,6 +614,15 @@ var SFItemTransformer = function () {
   }, {
     key: 'decryptItem',
     value: function decryptItem(item, keys) {
+
+      if ((item.content.startsWith("001") || item.content.startsWith("002")) && item.enc_item_key) {
+        // is encrypted, continue to below
+      } else {
+        // is base64 encoded
+        item.content = SFJS.crypto.base64Decode(item.content.substring(3, item.content.length));
+        return;
+      }
+
       // decrypt encrypted key
       var encryptedItemKey = item.enc_item_key;
       var requiresAuth = true;
@@ -677,13 +686,7 @@ var SFItemTransformer = function () {
           var isString = typeof item.content === 'string' || item.content instanceof String;
           if (isString) {
             try {
-              if ((item.content.startsWith("001") || item.content.startsWith("002")) && item.enc_item_key) {
-                // is encrypted
-                this.decryptItem(item, keys);
-              } else {
-                // is base64 encoded
-                item.content = SFJS.crypto.base64Decode(item.content.substring(3, item.content.length));
-              }
+              this.decryptItem(item, keys);
             } catch (e) {
               item.errorDecrypting = true;
               if (throws) {
